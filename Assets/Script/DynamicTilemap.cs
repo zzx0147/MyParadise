1043c867116a15fae9ddbf065967d4cdbd96b608
NaHoGyeom <zzx0147@gmail.com>
2019-07-15 10:37:06 +0900

    add Test Tile Image and help text for using git

---
+++ Assets/Script/DynamicTilemap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class DynamicTilemap : MonoBehaviour
+{
+    public TileBase temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+        tilemap.SetTile(new Vector3Int(0, 0, 0), temp);
+        tilemap.SetTile(new Vector3Int(1, 0, 0), temp);
+        tilemap.SetTile(new Vector3Int(1, 1, 0), temp);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
