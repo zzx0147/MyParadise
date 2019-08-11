73306cf9f9f778161c25b9842fea235de17be8c7
NaHoGyeom <zzx0147@gmail.com>
2019-08-11 21:51:04 +0900

    update csvLoader

---
+++ Assets/Script/ShopElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopElement : MonoBehaviour
+{
+
+    public Text Price;
+    public Image Thumnail;
+
+    public void SetElement(int price,Sprite thumnail)
+    {
+        Price.text = price.ToString();
+        Thumnail.sprite = thumnail;
+        
+    }
+
+}
---
+++ Assets/Script/ShopManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[,] result = CsvLoader.LoadCsvBy2DimensionArray("Csv/My_Paradise_Shop");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
